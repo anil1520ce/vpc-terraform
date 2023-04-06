233d91b35bfdc5e66333014cce6807ddcf14927f
anil1520ce <manilkumar.ce@gmail.com>
2023-04-06 17:02:44 +0530

    updated provide.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = var.region
 }
+terraform {
+  backend "s3" {
+    key            = "test/terraform.tfstate"
+    region         = "us-east-1"
+    bucket         = "anil-us-east-1-tfstate"
+    encrypt        = true
+  }
+}
